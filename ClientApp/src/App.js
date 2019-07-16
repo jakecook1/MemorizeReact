7f24c78805599a6cf5848418a2b244fbd6357a1d
jakecook1 <jake.cook1@gmail.com>
2019-07-17 11:35:14 +1200

    Moved some files in common folder

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
-import Layout from './components/Layout';
+import Layout from './components/Common/Layout';
 import Home from './components/Home';
 import About from './components/About';
 import './custom.css'
